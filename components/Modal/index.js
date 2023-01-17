b3d13ffddfcc778edb65a9b22cb6ee2fc1d7f546
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2023-01-18 04:46:49 +0500

    licence page ui finished

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Modal = ({modalClassName , children}) => {
+const Modal = ({modalClassName , modalWrapperClassName , children}) => {
   return (
-    <div className='w-screen h-screen bg-[#c4c4c4b3] fixed top-0 flex items-center'>
+    <div className={`w-screen h-screen bg-[#c4c4c4b3] fixed top-0 flex items-center ${modalWrapperClassName}`}>
         <div className={`mx-auto max-w-[1200px] w-full bg-[#fff] h-[530px] my-auto rounded-[20px] ${modalClassName}`}>
           {children}
         </div>
--- pages/licenses.js
+++ pages/licenses.js
@@ -11,9 +11,28 @@ import Button from "../components/Button/Button";
 import EquipmentCard from "../components/EquipmentCard/EquipmentCard";
 import ProductCard from "../components/ProductCard/ProductCard";
 import TabTitle from "../components/TabTitle/TabTitle"; 
+import Input from "../components/Input/Input";
+import Modal from "../components/Modal";
+import ModalCancelIcon from '../public/assets/images/modalCancel.svg'
+
 const Licenses = () => {
   return (
     <section>
+      <Modal modalClassName={'pt-[50px] pb-[75px] px-[50px]'} modalWrapperClassName={'hidden'}>
+        <div className="max-w-[760px] w-full ml-auto flex items-center justify-between mb-[75px]">
+          <h2 className="font-PoppinsBold text-[40px] leading-[140%] corder-[#fff] text-[#0F172A]">
+            Get 30-day free trial
+          </h2>
+          <Image src={ModalCancelIcon} width={50} height={50} alt="modal cancel icon" />
+        </div>
+        <div className="mx-auto max-w-[428px] w-full">
+          <Input inputType={'text'} wrapperClassName={'w-full mb-[44px]'} inputClassName={'text-[#94A3B8] w-full py-[20px] pl-[16px] placeholder:text-[#94A3B8] placeholder:font-PoppinsRegular placeholder:text-[14px] placeholder:leading-[180%] outline-none border-solid border border-[#94A3B8] rounded-[8px] bg-[#fff]'} placeholder={'Enter a name'} />
+          <Input inputType={'tel'} wrapperClassName={'w-full mb-[44px]'} inputClassName={'text-[#94A3B8] w-full py-[20px] pl-[16px] placeholder:text-[#94A3B8] placeholder:font-PoppinsRegular placeholder:text-[14px] placeholder:leading-[180%] outline-none border-solid border border-[#94A3B8] rounded-[8px] bg-[#fff]'} placeholder={'Enter a phone number'} />
+          <Button className={'py-[16px] w-full bg-[#FC7941] rounded-[10px] font-bold text-xl leading-[140%] poppins'}>
+            Request access
+          </Button>
+        </div>
+      </Modal>
       <div className="site-container">
         <h2 className="text-[32px] text-[#fff] font-[700] mt-[100px] mb-[40px]">Yoshop Licenses</h2>
 
