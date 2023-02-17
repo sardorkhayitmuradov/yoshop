aa3c533a81e91ca5e400f44f490350cb7a3d9606
shohjahon <komiljonovshohjahon@gmail.com>
2023-02-17 12:04:42 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import i18nextConfig from '../next-i18next.config';
 
-export default function Document() {
+export default function Document(props) {
   const currentLocale =
   // eslint-disable-next-line no-underscore-dangle
-  this.props.__NEXT_DATA__.locale || i18nextConfig.i18n.defaultLocale;
+  props.__NEXT_DATA__.locale || i18nextConfig.i18n.defaultLocale;
   return (
     <Html lang={currentLocale}>
       <Head />
