b87bb40cbc1de2e86796834341bf8cf258e6a3a7
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2023-02-21 20:43:03 +0500

    getStaticProps

--- pages/equipment/index.js
+++ pages/equipment/index.js
@@ -14,7 +14,7 @@ import {equipments} from '../../constants/equipments'
 import { useTranslation } from "react-i18next";
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations'
 
-export async function getServerSideProps({locale}){
+export async function getStaticProps({locale}){
   return{
     props: {
       ...(await serverSideTranslations(locale , ['common', 'header', 'footer'])),
